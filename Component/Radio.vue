9fc0410cfe1585fc8fcd839c66d126431cfc6cf1
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-31 18:46:15 +0700

    Add Radio.vue: component radio button tuỳ biến

---
+++ Component/Radio.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <VaRadio
+      v-for="option in options"
+      :key="option"
+      :option="option"
+      :name="name"
+      :model-value="modelValue"
+      @update:modelValue="$emit('update:modelValue', $event)"
+      class="mb-2"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: 'radio-group',
+    },
+  },
+};
+</script>
