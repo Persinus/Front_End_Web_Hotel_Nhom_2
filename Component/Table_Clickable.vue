a5555182c4a8b2bd4af08c6c04a70603c6975461
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-31 18:46:19 +0700

    Add Table_Clickable.vue: bảng dữ liệu có thể nhấn được từng dòng

---
+++ Component/Table_Clickable.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="va-table-responsive">
+    <table class="va-table va-table--clickable">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Country</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="user in users"
+          :key="user.id"
+        >
+          <td>{{ user.fullName }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.country }}</td>
+          <td>
+            <VaBadge
+              :text="user.status"
+              :color="user.status"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from 'vue'
+
+interface User {
+  id: number
+  fullName: string
+  email: string
+  country: string
+  status: string
+}
+
+const props = defineProps({
+  users: {
+    type: Array as PropType<User[]>,
+    required: true
+  }
+})
+</script>
+
+<style>
+.va-table-responsive {
+  overflow: auto;
+}
+</style>
