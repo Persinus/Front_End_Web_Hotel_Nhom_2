bfb8e2509b18e2a91f891a51560039a2e00bd040
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-31 18:46:14 +0700

    Add Pagination.vue: component phân trang có thể tái sử dụng

---
+++ Component/Pagination.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    totalPages: {
+      type: Number,
+      default: 15,
+    },
+    modelValue: {
+      type: Number,
+      default: 1,
+    },
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(val) {
+        this.$emit('update:modelValue', val);
+      },
+    },
+  },
+};
+</script>
+
+<template>
+  <VaPagination
+    v-model="value"
+    :pages="totalPages"
+    :visible-pages="4"
+    class="justify-center sm:justify-start"
+  />
+</template>
